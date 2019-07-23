f74f72583068370a8c752897d931c3063f9a8ef5
sanesco <santiagoescobar11z@gmail.com>
2019-07-22 23:40:57 -0500

    Changes title and paragraph

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>My first app</h1>
-      <p>This is my first app on React, it's funcy!</p>
+      <h1>Mi primera aplicación</h1>
+      <p>Esta es mi primera aplicación en React, está padrísimo!</p>
     </div>
   );
 }
